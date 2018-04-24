c4acb4cb4be5f6b68623f4b200ccc8ef72f2a9ba
Sam Vevang <sam.vevang@gmail.com>
2018-04-24 15:48:00 -0500

    no dns lookups in our tests

--- lib/service_monitor/ping_runner.rb
+++ lib/service_monitor/ping_runner.rb
@@ -19,18 +19,48 @@ module ServiceMonitor
       Net::Ping::HTTP.new(host, port=options.port)
     end
 
+    def print_ping(ping_time)
+        puts ping_time
+    end
+
     def call
       return -1 unless host_exists?
-      @start_time = Time.now
+      start!
 
+      results = []
       pinger = setup_pinger
-      ping_time = do_ping(pinger)
-      ping_time
+      num_intervals.times do |i|
+        break if finished?
+        ping_time = do_ping(pinger)
+        results.append(ping_time)
+        print_ping(ping_time)
+        sleep(seconds_until_interval(i+1))
+      end
+      # TODO return something real here
+      results.last
 
+    end
+
+    def seconds_until_interval(ping_offset)
+      sleep_time =  interval_begins_at(ping_offset) - Time.now
+      # in the case where the timeout is longer than the interval
+      if sleep_time < 0
+        0
+      else
+        sleep_time
+      end
+    end
+
+    def start!
+      @start_time ||= Time.now
     end
 
     def interval_begins_at(ping_offset)
       start_time + (options.interval * ping_offset)
+    end
+
+    def num_intervals
+      (options.duration / options.interval.to_f).floor
     end
 
     def do_ping(pinger)
--- spec/ping_runner_spec.rb
+++ spec/ping_runner_spec.rb
@@ -16,6 +16,10 @@ RSpec.describe ServiceMonitor::PingRunner do
   let(:host) { 'gitlab.com' }
   let(:ping_runner) { ServiceMonitor::PingRunner.new(host, options) }
 
+  before do
+    allow(ping_runner).to receive(:host_exists?) { true }
+  end
+
   context "#setup_pinger" do
     subject(:http_pinger) { ping_runner.setup_pinger }
 
@@ -25,6 +29,8 @@ RSpec.describe ServiceMonitor::PingRunner do
     context "Customized port changes pinger port ivars" do
       let(:options){
         options = ServiceMonitor::CLI.default_options
+        options.duration = duration
+        options.interval = interval
         options.port = 443
         options
       }
@@ -35,18 +41,22 @@ RSpec.describe ServiceMonitor::PingRunner do
   end
 
   context "PingRunner is invoked" do
-    before do
-      expect(ping_runner).to receive(:do_ping).with(Net::Ping) { 0.00001 }
-    end
 
     context "#do_ping" do
-      it "expects invocation of#do_ping" do
-        # verifies our stub in the before_hook
-        expect(ping_runner.call).to eq(0.00001)
+      it "pinger library instance expects invocation of #ping" do
+        # mock the pinger class from 'net/ping' and return a fixed duration
+        pinger = ping_runner.setup_pinger
+        expect(pinger).to receive(:ping).at_least(:once)
+        expect(pinger).to receive(:duration).at_least(:once) { 0.33333 }
+        expect(ping_runner.do_ping(pinger)).to eq(0.33333)
       end
     end
 
     context "#call" do
+      before do
+        expect(ping_runner).to receive(:do_ping).at_least(:once).with(Net::Ping) { 0.00001 }
+      end
+
       it "sets a start time when the runner is called" do
         expect(ping_runner.start_time).to be_nil
         ping_runner.call
@@ -58,46 +68,62 @@ RSpec.describe ServiceMonitor::PingRunner do
     context "#started?" do
       it "returns a boolean if the runner has been started" do
         expect(ping_runner.started?).to eq(false)
-        ping_runner.call
+        ping_runner.start!
         expect(ping_runner.started?).to eq(true)
       end
     end
 
-    context "PingRunner measures time" do
-      let(:interval) { 10 }
-      let(:duration) { 60 }
-      let(:start_time) { Time.local(2018, 1, 1, 0, 0, 0) }
+  end
 
-      before do
-        Timecop.freeze(start_time)
-        ping_runner.call
-        expect(ping_runner.finished?).to eq(false)
-      end
+  context "PingRunner measures time" do
+    let(:interval) { 10 }
+    let(:duration) { 60 }
+    let(:start_time) { Time.local(2018, 1, 1, 0, 0, 0) }
 
-      after do
-        Timecop.return
-      end
+    before do
+      Timecop.freeze(start_time)
+      ping_runner.start!
+    end
 
-      context "#finished?" do
+    after do
+      Timecop.return
+    end
 
-        it "returns false if the runner is within its duration" do
-          Timecop.freeze(start_time + 59.99999)
-          expect(ping_runner.finished?).to eq(false)
-        end
+    context "#start!" do
 
-        it "returns false if the runner's current time is equal to its duration" do
-          Timecop.freeze(start_time + 60)
-          expect(ping_runner.finished?).to eq(false)
-        end
+      it "is idemopotent across multiple invocations" do
+        expect(ping_runner.send(:start_time)).to eq(start_time)
 
-        it "returns true if the runner's current time is greater than its duration" do
-          Timecop.freeze(start_time + 60.00001)
-          expect(ping_runner.finished?).to eq(true)
-        end
+        # bump the time and restart to see that the old start time remains recorded
+        Timecop.freeze(start_time + 1)
+        ping_runner.start!
+
+        expect(ping_runner.send(:start_time)).to eq(start_time)
       end
 
     end
+
+    context "#finished?" do
+
+      it "returns false if the runner is within its duration" do
+        expect(ping_runner.finished?).to eq(false)
+        Timecop.freeze(start_time + 59.99999)
+        expect(ping_runner.finished?).to eq(false)
+      end
+
+      it "returns false if the runner's current time is equal to its duration" do
+        Timecop.freeze(start_time + 60)
+        expect(ping_runner.finished?).to eq(false)
+      end
+
+      it "returns true if the runner's current time is greater than its duration" do
+        Timecop.freeze(start_time + 60.00001)
+        expect(ping_runner.finished?).to eq(true)
+      end
+    end
+
     context "#interval_begins_at" do
+      # choose a funky interval
       let(:interval) { 2.5 }
       it "calculates when the next interval begins based on the runner start time and ping count" do
         expect(ping_runner.interval_begins_at(0)).to eq(start_time)
@@ -105,6 +131,27 @@ RSpec.describe ServiceMonitor::PingRunner do
         expect(ping_runner.interval_begins_at(3)).to eq(start_time + 7.5)
       end
     end
-  end
 
+    context "#num_intervals" do
+      it "calculates how many times the PingRunner will ping" do
+        # 10 second intervals over 60 seconds
+        expect(ping_runner.num_intervals).to eq(6)
+      end
+    end
+
+    context "seconds_until_interval" do
+
+      it "calculates how many seconds until the i'th interval begins" do
+        expect(ping_runner.seconds_until_interval(0)).to eq(0)
+        expect(ping_runner.seconds_until_interval(1)).to eq(10)
+        expect(ping_runner.seconds_until_interval(9)).to eq(90)
+      end
+
+      it "should return 0 for negative times" do
+        # something way past the current interval
+        Timecop.freeze(start_time + 59.99999)
+        expect(ping_runner.seconds_until_interval(1)).to eq(0)
+      end
+    end
+  end
 end
